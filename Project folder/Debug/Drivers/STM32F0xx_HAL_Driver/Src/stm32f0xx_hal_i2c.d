b6471ef6b95978644ff334e4d6bce41a460fd6a6
GMeeser <grant@meeser.co.za>
2018-05-19 12:20:59 +0200

    Add files via upload

---
+++ Project folder/Debug/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.d
@@ -0,0 +1,92 @@
+Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.o: \
+ ../Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Inc/stm32f0xx_hal_conf.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Inc/main.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f051x8.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/CMSIS/Include/core_cm0.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc_ex.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rtc.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rtc_ex.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h \
+ C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Inc/stm32f0xx_hal_conf.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Inc/main.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f051x8.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/CMSIS/Include/core_cm0.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc_ex.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rtc.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rtc_ex.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h:
+
+C:/Users/G.Meeser/Documents/Scratch\ Box/Design/Simulation-G21/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h:
